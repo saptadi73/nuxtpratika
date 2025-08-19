96d1aca852f4e3a010c58f249dc0ea17c278dff3
saptadi73 <saptadi.nurfarid@gmail.com>
2025-08-20 06:13:22 +0700

    Upload Video baru

--- app/component/HeroVideoSectionInd.vue
+++ app/component/HeroVideoSectionInd.vue
@@ -8,7 +8,7 @@
       playsinline
       poster="../assets/images/sibuk_pekerja.png"
     >
-      <source src="../assets/videos/sibuk_manufacture_video.mp4" type="video/mp4" />
+      <source src="../assets/videos/pratika_video_canva.mp4" type="video/mp4" />
       Your browser does not support the video tag.
     </video>
     <div class="absolute inset-0 bg-black/60 md:bg-black/40"></div>
